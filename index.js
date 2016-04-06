8d8fbf598211f2516d07e8a9245480eb033d3390
Benjamin E. Coe <bencoe@gmail.com>
2016-04-06 16:49:18 -0700

    feat(polling): poller now polls and updates UI

--- index.js
+++ index.js
@@ -1,27 +1,71 @@
 var $ = require('jquery')
+var Mustache = require('mustache')
 
-function AnnotationPoller (loaded) {
-  var _this = this
+function AnnotationPoller (opts) {
+  this.pollInterval = opts.pollInterval || 3000
   this.endpoint = '/api/v1/annotations.js'
   this.annotations = {}
+  this.template = '<li id="annotation-{{id}}" style="{{status}}"><span>{{description}}</span><a href="{{{external-link}}}">{{external-link-text}}</a></li>'
+  this.addonSelector = '#npm-addon-box'
+}
+
+AnnotationPoller.prototype.start = function (loaded) {
+  var _this = this
+  var updating = false
 
   $(document).ready(function () {
-    _this.load(function () {
-      return loaded()
-    })
+    this.interval = setInterval(function () {
+      if (updating) return
+      updating = true
+
+      _this.getAnnotations(function () {
+        updating = false
+        _this.renderAnnotations()
+        if (loaded) {
+          loaded()
+          loaded = null
+        }
+      })
+    }, this.pollInterval)
   })
 }
 
-AnnotationPoller.prototype.load = function (cb) {
+AnnotationPoller.prototype.stop = function () {
+  clearInterval(this.interval)
+}
+
+AnnotationPoller.prototype.getAnnotations = function (cb) {
   var _this = this
   $.getJSON(this.endpoint, function (data) {
     $.each(data, function (i, annotation) {
-      _this.annotations[annotation.uri] = annotation
+      _this.annotations[annotation.id] = annotation
     })
     return cb()
   })
 }
 
-module.exports = function (loaded) {
-  return new AnnotationPoller(loaded)
+AnnotationPoller.prototype.renderAnnotations = function () {
+  var _this = this
+  var annotation = null
+  var annotationElement = null
+  var newAnnotationElement = null
+  var addonBox = $(this.addonSelector)
+
+  Object.keys(this.annotations).forEach(function (key) {
+    annotation = _this.annotations[key]
+    if (annotation._rendered) return
+
+    annotationElement = $('#annotation-' + annotation.id)
+    newAnnotationElement = Mustache.render(_this.template, annotation)
+    if (annotationElement.length) {
+      annotationElement.replaceWith(newAnnotationElement)
+    } else {
+      addonBox.append(newAnnotationElement)
+    }
+    annotation._rendered = true
+  })
+}
+
+module.exports = function (opts) {
+  return new AnnotationPoller(opts)
 }
--- test.js
+++ test.js
@@ -1,21 +1,27 @@
-/* global describe, it */
+/* global describe, it, beforeEach */
 
 require('jsdom-global')()
-document.body.innerHTML = '<ul class="box" id=""></ul>'
 
 var annotationPoller = require('./')
 var endpoint = '/api/v1/annotations.js'
-var jQuery = require('jquery')
+var $ = require('jquery')
 
-require('jquery-mockjax')(jQuery)
-jQuery.mockjaxSettings.logging = false
+require('jquery-mockjax')($)
+$.mockjaxSettings.logging = false
+
+require('chai').should()
 
 describe('annotation-poller', function () {
+  beforeEach(function () {
+    document.body.innerHTML = '<ul class="box" id="npm-addon-box"></ul>'
+    $.mockjax.clear()
+  })
+
   it('grabs an initial list of annotations when the page loads', function (done) {
-    jQuery.mockjax({
+    $.mockjax({
       url: endpoint,
       responseText: [{
-        uri: 'foo-package',
+        id: 'abc-123-abc',
         status: 'warn',
         'status-message': 'module not yet scanned',
         description: 'foo security integration',
@@ -25,10 +31,114 @@ describe('annotation-poller', function () {
       }]
     })
 
-    var poller = annotationPoller(function () {
-      poller.annotations['foo-package'].status = 'warn'
-      poller.annotations['foo-package'].description = 'foo security integration'
+    var poller = annotationPoller({pollInterval: 50})
+    poller.start(function () {
+      poller.annotations['abc-123-abc'].status = 'warn'
+      poller.annotations['abc-123-abc'].description = 'foo security integration'
+      poller.stop()
       return done()
+    })
+  })
+
+  it('renders a new annotation', function (done) {
+    $.mockjax({
+      url: endpoint,
+      responseText: [{
+        id: 'abc-123-abc',
+        status: 'warn',
+        'status-message': 'module not yet scanned',
+        description: 'my awesome integration',
+        'external-link': 'http://example.com/foo-package/audit',
+        'external-link-text': 'start audit',
+        timeout: 20
+      }]
+    })
+
+    var poller = annotationPoller({pollInterval: 50})
+    poller.start(function () {
+      $('ul li').length.should.equal(1)
+      $('ul li').text().should.match(/my awesome integration/)
+      poller.stop()
+      return done()
+    })
+  })
+
+  it('replaces an existing annotation with new data from an integration', function (done) {
+    $.mockjax({
+      url: endpoint,
+      responseText: [{
+        id: 'abc-123-abc',
+        status: 'warn',
+        'status-message': 'module not yet scanned',
+        description: 'my awesome integration',
+        'external-link': 'http://example.com/foo-package/audit',
+        'external-link-text': 'start audit',
+        timeout: 20
+      }]
+    })
+
+    var poller = annotationPoller({pollInterval: 50})
+    poller.start(function () {
+      $.mockjax.clear()
+      $.mockjax({
+        url: endpoint,
+        responseText: [{
+          id: 'abc-123-abc',
+          status: 'green',
+          'status-message': 'module scanned',
+          description: 'my awesome integration',
+          'external-link': 'http://example.com/foo-package/audit',
+          'external-link-text': 'view details',
+          timeout: 20
+        }]
+      })
+
+      setTimeout(function () {
+        $('ul li').length.should.equal(1)
+        $('ul li').text().should.match(/view details/)
+        poller.stop()
+        return done()
+      }, 1000)
+    })
+  })
+
+  it('adds an additional annotation from a new integration', function (done) {
+    $.mockjax({
+      url: endpoint,
+      responseText: [{
+        id: 'abc-123-abc',
+        status: 'warn',
+        'status-message': 'module not yet scanned',
+        description: 'my awesome integration',
+        'external-link': 'http://example.com/foo-package/audit',
+        'external-link-text': 'start audit',
+        timeout: 20
+      }]
+    })
+
+    var poller = annotationPoller({pollInterval: 50})
+    poller.start(function () {
+      $.mockjax.clear()
+      $.mockjax({
+        url: endpoint,
+        responseText: [{
+          id: 'fed-234-abc',
+          status: 'green',
+          'status-message': 'module licensed',
+          description: 'my second integration',
+          'external-link': 'http://example.com/foo-package/audit',
+          'external-link-text': 'view details',
+          timeout: 20
+        }]
+      })
+
+      setTimeout(function () {
+        $('ul li').length.should.equal(2)
+        $('ul li:first').text().should.match(/my awesome integration/)
+        $('ul li:last').text().should.match(/my second integration/)
+        poller.stop()
+        return done()
+      }, 1000)
     })
   })
 })
